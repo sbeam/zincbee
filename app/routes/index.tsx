1686b60636fa9daabbccab813308ca00d8aba3cb
Sam Beam <sbeam@syxyz.net>
2022-10-20 16:01:16 -0400

    currency format, cleanup

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,21 +1,21 @@
 import { Form, Outlet, useLoaderData } from '@remix-run/react'
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react"
 import { FormattedDate } from '~/components/date'
-import { classNames } from 'primereact/utils';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-import { useQueryClient, useQuery } from 'react-query'
+import { classNames } from 'primereact/utils'
+import { DataTable } from 'primereact/datatable'
+import { Column } from 'primereact/column'
+import { useQuery } from 'react-query'
 
-import theme from "primereact/resources/themes/lara-dark-indigo/theme.css";  //theme
-import pr from "primereact/resources/primereact.min.css";                  //core css
-import icons from "primeicons/primeicons.css";                                //icons
+import theme from "primereact/resources/themes/lara-dark-indigo/theme.css"  //theme
+import pr from "primereact/resources/primereact.min.css"                  //core css
+import icons from "primeicons/primeicons.css"                                //icons
 
 export function links() {
   return [
     { rel: "stylesheet", href: pr },
     { rel: "stylesheet", href: theme },
     { rel: "stylesheet", href: icons },
-  ];
+  ]
 }
 
 async function loader({ start, end }: any) {
@@ -33,22 +33,34 @@ const CurrentQuote = ({ symbol }: CurrentQuoteProps) => {
   // to extend apca to get the latest trade
   const { isLoading, isError, data, error } = useQuery(['latest', symbol], async () => {
     console.log('fetching latest quote for ', symbol)
-    return await fetch(`http://localhost:3001/latest?sym=${symbol}`).then(res => res.json())
+    const response = await fetch(`http://localhost:3001/latest?sym=${symbol}`)
+    if (!response.ok) {
+      console.log('Network error', response.status)
+      throw new Error('Network error')
+    }
+    return response.json()
   })
   if (isLoading) return <p>...</p>
   if (isError && error instanceof Error) return <p>{error.message}</p>
-  console.log(symbol, data)
 
   return (
     <div className="latest-trade">
-      {data.ask_price} / {data.bid_price}
+      {currencyFormat(data.ask_price)} / {currencyFormat(data.bid_price)}
     </div>
   )
 }
 
+const currencyFormat = (value: number | bigint) => {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  })
+  return formatter.format(value)
+}
+
 export default function Index() {
-  const [positions, setPositions] = useState([]);
-  const [selectedRow, setSelectedRow] = useState(null);
+  const [positions, setPositions] = useState([])
+  const [selectedRow, setSelectedRow] = useState(null)
 
   const rowClass = (data: any) => {
     return {
@@ -81,12 +93,12 @@ export default function Index() {
         <Column field="sym" header="Symbol"></Column>
         <Column field="created_at" header="Entered" body={(row) => <FormattedDate isoString={row.created_at} />}></Column>
         <Column field="qty" header="Quantity"></Column>
-        <Column field="filled_avg_price" header="Price"></Column>
-        <Column field="cost_basis" header="Cost Basis"></Column>
-        <Column header="Current" body={(row) => <CurrentQuote symbol={row.sym} />}></Column>
+        <Column field="filled_avg_price" header="Price" body={(row) => currencyFormat(row.filled_avg_price)} />
+        <Column field="cost_basis" header="Cost Basis" body={(row) => currencyFormat(row.filled_avg_price)} />
+        <Column field="current" header="Current" body={(row) => <CurrentQuote symbol={row.sym} />}></Column>
       </DataTable>
     </div>
     <Outlet />
     </>
-  );
+  )
 }
