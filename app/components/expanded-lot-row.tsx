2dd02246caddcb281e84c68d74ee02c98fa39524
Sam Beam <sbeam@syxyz.net>
2022-12-16 17:14:27 -0500

    improve details panel a bit

--- app/components/expanded-lot-row.tsx
+++ app/components/expanded-lot-row.tsx
@@ -2,6 +2,7 @@ import LiquidateForm from '~/components/liquidate-form'
 import { useState } from "react"
 import { Button } from 'primereact/button'
 import { Message, MessageSeverityType } from 'primereact/message'
+import { Panel } from 'primereact/panel'
 import currencyFormat from '~/utils/currency-format'
 import { useMutation } from "react-query"
 import { FormattedDate } from '~/components/date'
@@ -91,35 +92,47 @@ export const ExpandedLotRow = (row: any) => {
 
   const side = row.position_type === 'Long' ? 'Bought' : 'Sold Short'
 
+  const rr = (row.filled_avg_price - row.stop_price) / (row.limit_price - row.filled_avg_price)
+
   const proceeds = row.status === 'Disposed' ? row.disposed_fill_price * row.qty : 0
   const gain = row.status === 'Disposed' ? proceeds - row.cost_basis : 0
+  const pct_gain = (gain / row.cost_basis) * 100
+
+  const slip = (row.disposed_fill_price - row.stop_price)
+  const slip_pct = slip / row.disposed_fill_price * 100
+
+  const detailHeader = <span>{row.position_type} {row.qty} <strong className="text-lg">{row.sym}</strong></span>
+
+  const firstColStyle = { width: '10em' }
 
   // TODO https://www.primefaces.org/primereact/panel/
   return (
-    <div className="expanded-row">
-      <div className="details">
-        <h3>Details</h3>
-        <div>{row.position_type} {row.qty} <strong>{row.sym}</strong></div>
-        <table>
-          <tbody>
-            <tr>
-              <td>{side}:</td>
-              <td>{currencyFormat(row.filled_avg_price)}</td>
-              <td><FormattedDate isoString={row.created_at} /></td>
-              <td>{currencyFormat(row.cost_basis)}</td>
-            </tr>
-              {row.status === 'Disposed' && <tr>
-                <td>{row.dispose_reason}:</td>
-                <td>{currencyFormat(row.disposed_fill_price)}</td>
-                <td><FormattedDate isoString={row.disposed_at} /></td>
-                <td>{currencyFormat(proceeds)}</td>
-              </tr>}
-              {row.status === 'Disposed' && <tr>
-                <td colSpan={3} align="right">Net:</td>
-                <td>{currencyFormat(gain)}</td>
-              </tr>}
-            </tbody>
-        </table>
+    <div className="flex flex-row flex-wrap">
+      <div className="flex flex-col w-6">
+        <Panel header={detailHeader}>
+          <div className="grid">
+            <div className="col-fixed text-right" style={firstColStyle}>R/R:</div>
+            <div className="col">{rr.toFixed(1)}</div>
+          </div>
+          <div className="grid">
+            <div className="col-fixed" style={firstColStyle}><FormattedDate isoString={row.created_at} /></div>
+            <div className="col">{side}@ {currencyFormat(row.filled_avg_price)}</div>
+          </div>
+          {row.status === 'Disposed' && <>
+            <div className="grid">
+              <div className="col-fixed" style={firstColStyle}><FormattedDate isoString={row.disposed_at} /></div>
+              <div className="col">{row.dispose_reason}@ {currencyFormat(row.disposed_fill_price)}</div>
+            </div>
+            <div className="grid">
+              <div className="col-fixed text-right" style={firstColStyle}>Slip:</div>
+              <div className="col">{currencyFormat(slip)} ({slip_pct.toFixed(2)}%)</div>
+            </div>
+            <div className="grid">
+              <div className="col-fixed text-right" style={firstColStyle}>Realized:</div>
+              <div className="col">{currencyFormat(gain)} ({pct_gain.toFixed(2)}%)</div>
+            </div>
+          </>}
+        </Panel>
       </div>
 
       <div className="actions">
