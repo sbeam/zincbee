943e2bd3227117edc08d3855fb8e617012cd5e87
Sam Beam <sbeam@syxyz.net>
2023-01-27 21:35:08 -0500

    can delete empty bucket, like S3

--- app/components/bucket-tabs-view.tsx
+++ app/components/bucket-tabs-view.tsx
@@ -7,26 +7,90 @@ import { InputText } from 'primereact/inputtext'
 import { Toast } from 'primereact/toast'
 import { Menu } from 'primereact/menu'
 import { Button } from 'primereact/button'
+import { ConfirmPopup, confirmPopup } from 'primereact/confirmpopup'
 
 import LotsTable from '~/components/lots-table'
 
-const BucketMenu = ({deletable}: {deletable: boolean}) => {
+interface BucketProps {
+  name: string,
+  rowid: string,
+  lot_count: number,
+}
+
+const BucketMenu = ({bucket, refetch}: {bucket: BucketProps, refetch: Function}) => {
+  const errorToast = useRef<Toast>(null)
   const menu = useRef<Menu>(null)
 
+  const { mutate: deleteBucket } = useMutation({
+    mutationFn: async (bucketName: string) => {
+      const response = await fetch("http://localhost:3001/bucket", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({name : bucketName }),
+      })
+      const result = await response.json()
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText} - ${result.error}`)
+      }
+      return result
+    },
+    onSuccess: () => refetch(),
+  })
+
+  const confirmDeleteBucket = (event: any) => {
+    confirmPopup({
+      message: "Are you sure you want to delete this bucket?",
+      target: event.currentTarget,
+      icon: 'pi pi-info-circle',
+      acceptClassName: 'p-button-danger',
+      accept: async () => {
+        const result = deleteBucket(bucket.name, { onError: (e: any) => {
+          errorToast.current?.show({severity: 'error', summary: 'Error', detail: e.message, life: 3000})
+        }})
+        return result
+      }
+    })
+  }
+
   const items = [
-    { label: 'Rename', icon: 'pi pi-fw pi-pencil', command: (_e) => { console.log('mm', _e)} },
+    { label: 'Rename', icon: 'pi pi-fw pi-pencil', command: (_e: any) => { console.log('mm', _e)} },
   ]
-  if (deletable) {
-    items.push({ label: 'Delete', icon: 'pi pi-fw pi-trash', command: (_e) => { console.log('mm', _e)} })
+  if (bucket.lot_count === 0) {
+    items.push({ label: 'Delete', icon: 'pi pi-fw pi-trash', command: confirmDeleteBucket })
   }
 
   return (
     <div className="bucketMenu">
+      <Toast ref={errorToast} position="top-right" />
+      <ConfirmPopup />
       <Menu popup model={items} ref={menu} />
       <Button label="" icon="pi pi-cog" className="p-button-text p-button-rounded p-button-sm" onClick={(e: any) => menu.current?.toggle(e)} />
     </div>
   )
 }
+
+const usePostBucket = (op: React.RefObject<OverlayPanel>, refetch: Function) => (
+  useMutation({
+    mutationFn: async (newBucketName: string) => {
+      const response = await fetch(`http://localhost:3001/bucket`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name: newBucketName})
+      })
+      const result = await response.json()
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText} - ${result.error}`)
+      }
+      return result
+    },
+    onSuccess: () => { op.current?.hide(); refetch() },
+  })
+)
+
 
 const BucketTabsView = ({ onChange }: { onChange : Function} ) => {
   const [bucketIndex, setBucketIndex] = useState(0)
@@ -46,27 +110,11 @@ const BucketTabsView = ({ onChange }: { onChange : Function} ) => {
     refetchInterval: Infinity,
     cacheTime: Infinity,
     onSuccess: (data) => {
-      onChange(data[bucketIndex].rowid)
+      data[bucketIndex] ? onChange(data[bucketIndex].rowid) : chooseBucket(data.length - 1)
     }
   })
 
-  const { mutate: postBucket } = useMutation({
-    mutationFn: async (newBucketName: string) => {
-      const response = await fetch(`http://localhost:3001/bucket`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({name: newBucketName})
-      })
-      const result = await response.json()
-      if (!response.ok) {
-        throw new Error(`Error: ${response.statusText} - ${result.error}`)
-      }
-      return result
-    },
-    onSuccess: () => { op.current?.hide(); refetch() },
-  })
+  const { mutate: postBucket } = usePostBucket(op, refetch)
 
   if (isLoading) return <p>...</p>
   if (isError && error instanceof Error) return <p>{error.message}</p>
@@ -97,7 +145,7 @@ const BucketTabsView = ({ onChange }: { onChange : Function} ) => {
       {buckets.map((bucket: { rowid: string, name: string, lot_count: number }) => {
         return (
           <TabPanel header={bucket.name} key={bucket.rowid} leftIcon="pi pi-fw pi-home">
-            <BucketMenu deletable={bucket.lot_count == 0} />
+            <BucketMenu bucket={bucket} refetch={refetch} />
             <LotsTable bucket={bucket.rowid} />
           </TabPanel>
         )
